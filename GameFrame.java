2872c74f4c67d0dbcf54047ec909108837e90f95
VinceSeely <vince@vinceseely.com>
2015-11-11 08:06:08 -0600

    updates on size of frame

--- GameFrame.java
+++ GameFrame.java
@@ -26,6 +26,9 @@ public class GameFrame extends java.awt.Frame {
    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
    private void initComponents() {
 
+      setMaximumSize(new java.awt.Dimension(700, 700));
+      setMinimumSize(new java.awt.Dimension(700, 700));
+      setPreferredSize(new java.awt.Dimension(700, 700));
       addWindowListener(new java.awt.event.WindowAdapter() {
          public void windowClosing(java.awt.event.WindowEvent evt) {
             exitForm(evt);
